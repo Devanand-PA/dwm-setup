bd18a10fe848a26bae8d34909a78a43166678587
Devanand-PA <112339148+Devanand-PA@users.noreply.github.com>
2022-09-06 22:08:53 +0530

    Add files via upload

--- dwm/config.h
+++ dwm/config.h
@@ -5,12 +5,12 @@ static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=16" };
-static const char dmenufont[]       = "monospace:size=16";
+static const char *fonts[]          = { "monospace:size=14"};
+static const char dmenufont[]       = "monospace:size=14";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { "#000000", "#cccccc", "#cccccc" },
-	[SchemeSel]  = { "#000000", "#aa4444",  "#cccccc"  },
+	[SchemeNorm] = { "#ffffff", "#111111", "#111111" },
+	[SchemeSel]  = { "#000000", "#aa4444",  "#111111"  },
 };
 
 /* tagging */
@@ -52,12 +52,14 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", "#cccccc", "-nf", "#000000", "-sb", "#aa4444", "-sf", "#000000", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", "#111111", "-nf", "#ffffff", "-sb", "#aa4444", "-sf", "#000000", NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *WebBrowser[] = { "firefox", NULL };
 
+static const char *FileBrowser[] = { "pcmanfm", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_space,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -68,7 +70,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,	                XK_w,      killclient,     {0} },
+	{ MODKEY,	                XK_q,      killclient,     {0} },
+	{ MODKEY,			XK_w,	   spawn,	   {.v = WebBrowser } },
+	{ MODKEY,			XK_v,	   spawn,	   {.v = FileBrowser } },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
