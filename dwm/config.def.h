df472b3d9a5e62ec4ab739b3c6759e371943ec54
Devanand-PA <112339148+Devanand-PA@users.noreply.github.com>
2022-09-08 15:39:06 +0530

    Add files via upload

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -54,14 +54,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", "#111111", "-nf", "#ffffff", "-sb", "#aa4444", "-sf", "#000000", NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *WebBrowser[] = { "firefox", NULL };
+static const char *WebBrowser[] = { "chromium", NULL };
 
 static const char *FileBrowser[] = { "pcmanfm", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,		XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -71,8 +71,8 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,	                XK_q,      killclient,     {0} },
-	{ MODKEY,			XK_w,	   spawn,	   {.v = WebBrowser } },
 	{ MODKEY,			XK_v,	   spawn,	   {.v = FileBrowser } },
+	{ MODKEY,			XK_b,	   spawn,	   {.v = WebBrowser } },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
