393a4401e510f4ef48e3bc814317d4729e47948c
Devanand-PA <112339148+Devanand-PA@users.noreply.github.com>
2022-10-13 20:42:25 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 sudo pacman -S gcc make libxinerama libxft fontconfig xorg-xinit vim links sxiv mpv yt-dlp fzf curl wget base-devel
 sudo apt install gcc make libxinerama-dev libxft-dev xinit vim links sxiv mpv fzf curl wget 
 cat bashrc >> ~/.bashrc
-cp 30-touchpad.conf /etc/X11/xorg.conf.d/
+sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d/
 cd dwm
 sudo make clean install
 cd ..
